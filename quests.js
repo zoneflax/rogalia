effd0d501aa13d96a89e29dc46ad3f281d623138
TatriX <tatrics@gmail.com>
2015-01-13 22:16:28 +0300

    New sprites

---
+++ quests.js
@@ -0,0 +1,7 @@
+//tmp stub
+var quest = {
+    name: "Citizen's tribute",
+    begin: "Bring me some food and I'll give you status points.",
+    description: "Bring any food",
+    end: "Good job. Come back tommorow.",
+}
