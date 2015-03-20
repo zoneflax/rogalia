6d0952d35821b02227e293a4fc72deef4e4cf71a
TatriX <tatrics@gmail.com>
2015-03-20 21:35:13 +0300

    Added talks t10n

---
+++ talks.en.js
@@ -0,0 +1,65 @@
+function Talks() {
+    return {
+	stories: {
+	    "Oldman": "Everybody knows the Old one went totally nuts, the only thing he knows is sitting there like a king ang banging a floor with his staff when it sounds. The head of village is voodoo. He wizzez shit and shit happens. So, if he sees you fuzzing around with the night forestess through his bird eye, you\'ll be punished even if you\'ve never seen any forestess before.", 
+	    "Outlaws":"People say there's outlaws in the forest making families, breaking houses, burning fields, building they own buildings. That's why there's watch on a hill with all that bows and iron forks. So if you even think of taking into messing around, you\'ll be met with nothing of bread and meat. If you wanna met them, so go, if you know where to go. They put a secret signs on a trees and stones, smoke a fumes over the forest, mock birds. But they likely will eat the whole of you and make a cup of your skull.",
+	    "The forest": "So, you may live there if you wanna. If you build the house and be fine with this, you may look further. There's lots of food in the forest, and herbal stuff too. Beasts mostly ain't frightened, but beasts are different, if you catch my drift. But you can deal with them all, if the Spirits let you. There's another creatures, and not every one of them is pleasant and safe like white forestess or the caddis. If you ain't wary you'll lose the health, the soul and probably the life.",
+	    "People": "You can get to public road or the River and move to the fields and town. But nobody welcome you if you have nothing to trade. You'll just blink and there, you're a soldier, or a slave, or even dead. People sometimes are more dangerous than a beasts. So grow your roots in the Forest, learn a craft, trade things to anybody, protect your land, prepare yourself to be a citizen. They say some noble persons, even someone from Imperial Knights, they\'re moved out from Forest. And see, now they own a lands! They even say the ancestor of the Emperor, let the Spirits save him, was... Well, this was a long, long time ago.",
+	    "Things": "They also say the noble ones have a special voodoo staffs for range kill, and self-propelled carts, and men who ride iron Sky Flyers and drop the barrels of Marshy Spirits. There's lots of wizardry in the world! But you'll never see them, not you. You can't handle a day walk. You're nothing of a person. They even rob your name from you, so you shall get a nickname. Ain't you developed it yet? Really? Tell me.",
+	},
+	rules: {
+	    presents: "I'll take the longest row of atoms and give you a suitable present. The rows are: ROGALIK, OGALIK, GALIK, ALIK, LIK, IK, K"
+	},
+	"charles": [
+	    "Hey, folk!",
+	    "Is that you again? But, I sure understand there in the back country nobody but me can handle a good conversation. So, lets start from the beginning.",
+	    "I'm Charles, the officer of local Imperial Chancery. Cool, huh? Don't ask me why am I delivered to this boonies, but I'd start to find a pleasure with life like this.",
+	    "You could take a rest or trade your stuff to others in this town.",
+	    "People gathered here are very different, so I look for the order as the only present of the power.",
+	    "So you can:",
+	    {
+	        "Bank": "Keep your savings here, you know. I'm a local banker under the Imperial protection, so I'll keep them best.",
+	        "Set citizenship": "Tell me, what fraction are you belong to? The Emperor ain't friends with the Confederation, but you can feel safe here, I told you different people are gathered there. Nobody will hurt you for the party you belong. In the city at least.",
+	        "Get claim": "Get your own claim. Being put on a homestead, it will assign this area to you and you'll can bring your goods to Empire with no fear of somebody steals your hens.",
+	        "Quest": "Help your fraction comrades to survive here. This will rise you up in their eyes.",
+	        "Talk": "At last we can keep a small talk."
+	    }
+	],
+	"shot": [
+	    "Hey handsome, I'm Shot, but if you gotta money, I'll take a long for you *giggle*.",
+	    "Our pub \"The dancing coyot\" is the only place in town you should pay attention for. Even if Charles says different.",
+	    "We've got seeds, foods, water and something hot in the next room. I think you'll get into it with no help.",
+	    {
+	        "Buy": "Buy food or seeds.",
+	        "Drink water": "Drink water (5 silvers).",
+	        "Quest": "Take quest.",
+	    }
+	],
+	"margo": [
+	    "Hey sweety, don't pass by!",
+	    "Let's mess around, you know who I am, do you? You call us «mulier stupri», but I like «night butterfly» more, I look like a butterfly, don't I? Silky wings, body lightness, incredible beauty, but the heads are... I give pleasure to people, a special pleasure.",
+	    "So, do you come here to stare or even this big guy needs fun sometimes?",
+	    {
+	        "Buy sex": "Buy sex (10 gold)",
+	        "Quest": "Take quest.",
+	    }
+	],
+	"bruno": [
+	    "Um... Hey, guy, ain't you confused me with someone else? I'm not a babysitter, you know.",
+	    "Or... Ha! Um, well, welcome to my shop. I've got there... er... Things. The things every tailor needs, and there in the next room I've got my beloved smithy.",
+	    "So, you're like a newbie there, so you can use my tools for free. But don't broke anything! I'll rip your skin off and make a sacs of it! Um... Or I'll just write you a fine. We'll see.",
+	    "Well, ask me if you need something.",
+	    {
+	        "Buy": "Buy stones",
+	        "Quest": "Take quest.",
+	    }
+	],
+	"vendor": [
+	    "Hello!",
+	    {
+	        "Buy": "Buy a couple of things. You won't regret.",
+	        "Sell": "Perhaps you'll trade me some stuff I need?",
+	    },
+	],
+    };
+}
