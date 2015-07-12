28248e5e7b03f3bcfaf891e5b03fbbb231322840
TatriX <tatrics@gmail.com>
2015-07-12 12:43:36 +0300

    Fixed typos

--- tests/chat/ring.js
+++ tests/chat/ring.js
@@ -4,26 +4,25 @@ define(function () {
     var message = "No one cares about this text";
 
     // if we push() text into ring then last() must return that message
-    QUnit.test("CharRing.push", function(accert){
+    QUnit.test("ChatRing.push", function(accert){
         ring.push(message);
         accert.equal(ring.last(), message);
     });
 
     // now lets pop it, using prev()
-    QUnit.test("CharRing.prev", function(accert) {
+    QUnit.test("ChatRing.prev", function(accert) {
         accert.equal(ring.prev(), message);
         // calling it again must not change anything
         accert.equal(ring.prev(), message);
     });
 
     // we now have exactly one message in the ring, so next() must return nothing
-    QUnit.test("CharRing.next", function(accert) {
+    QUnit.test("ChatRing.next", function(accert) {
         accert.equal(ring.next(), "");
     });
 
-
     // okay, let's test several messages
-    QUnit.test("CharRing.multiple-prev", function(accert) {
+    QUnit.test("ChatRing.multiple-prev", function(accert) {
         ring.push("one");
         ring.push("two");
         ring.push("three");
@@ -33,15 +32,15 @@ define(function () {
         accert.equal(ring.prev(), message);
     });
 
-    QUnit.test("CharRing.multiple-next", function(accert) {
+    QUnit.test("ChatRing.multiple-next", function(accert) {
         accert.equal(ring.next(), "one");
         accert.equal(ring.next(), "two");
         accert.equal(ring.next(), "three");
         accert.equal(ring.next(), "");
     });
 
-    // now let's test backing of non-pushed message
-    QUnit.test("CharRing.save", function(accert) {
+    // now let's test backup of non-pushed message
+    QUnit.test("ChatRing.save", function(accert) {
         ring.push(message);
         var unsaved = "this text was not saved";
         ring.save(unsaved);
