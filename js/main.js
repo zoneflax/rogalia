a9ac5666be9a26168d02f345c31d7ae2153d514c
TatriX <tatrics@gmail.com>
2015-08-30 12:45:41 +0300

    IE redirect

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 "use strict";
-var game = new Game();
+//IE detector
+if ('\v'=='v') {
+    alert("Internet Explorer не поддерживается");
+    document.location = "http://rogalik.tatrix.org/wiki/index.php/%D0%97%D0%B0%D0%BF%D1%83%D1%81%D0%BA_%D0%BA%D0%BB%D0%B8%D0%B5%D0%BD%D1%82%D0%B0_%D0%B8%D0%B3%D1%80%D1%8B";
+} else {
+    var game = new Game();
+}
