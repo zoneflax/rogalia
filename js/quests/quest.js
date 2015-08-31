a900d2aee877a557a4d41326fd7317a4ff7abc90
TatriX <tatrics@gmail.com>
2015-08-31 17:57:10 +0300

    Revert "Updated quests"
    This reverts commit aa543345c045bfe545bf77bcff5e4906db96d84e.

--- js/quests/quest.js
+++ js/quests/quest.js
@@ -1,201 +1,146 @@
 "use strict";
-//tmp stub
-function Quests() {
-    return {
-        "hello": {
-            name: {
-                "en": "",
-                "ru": "Тёплый приём"
-            },
-            desc: {
-                "en": "",
-                "ru": "Ты решил у нас задержаться? Хорошо, но тебе придется построить себе дом, как минимум. Для этого тебе понадобится инструмент. В этом деле тебе поможет местный шахтер, Ахпер, беги к нему. ",
-            },
-            final: {
-                "en": "",
-                "ru": "Здарова, коль не шутишь. Тебя Чарльз прислал, или ты просто, мимо проходил, да решил заглянуть? В любом случае, добро пожаловать в мою камнеломню.",
-            },
-        },
-        "tour-1": {
-            name: {
-                "en": "",
-                "ru": "Создаем кирку",
-            },
-            desc: {
-                "en": "",
-                "ru": "Итак, давай сразу к делу, ибо времени у меня мало. В первую очередь, чтобы начать что-то делать, тебе понадобится кирка - с нее и начнем. Беги в лес, сорви пару суков с деревьев, пару веток и сделай себе кирку. Камней можешь у меня купить.",
-            },
-            tip: {
-                "en": "",
-                "ru": "ПКМ по деревьям даст возможность сорвать сук и ветку. Полученные сук и ветку надо обламать через пкм в инвентаре. Клик по молоту справа вызовет окно крафта, вводим в поиск \"острый камень\" или находим его в \"обработка камня\". Компоненты для него покупаем или находим в природе. Получив острый камень делаем Каменную Кирку.",
-            },
-            final: {
-                "en": "",
-                "ru": "Хорошая работа, старина. С помощью этого инструмента ты сможешь откалывать камни от булыжников и валунов.",
-            },
-        },
-        "tour-2": {
-            name: {
-                "en": "",
-                "ru": "Создаем нож",
-            },
-            desc: {
-                "en": "",
-                "ru": "Кирка - это здорово. Но все знают, что без хорошего ножа в наше время не обойтись. Тебе нужен нож, сделай его! ",
-            },
-            tip: {
-                "en": "",
-                "ru": "Нож и Кирка должны находиться в руках, для возможности взаимодействия с камнями и тушками животных.",
-            },
-            final: {
-                "en": "",
-                "ru": "Отлично! Теперь ты можешь разделывать туши животных и еще много полезного.",
-            },
-        },
-        "tour-3": {
-            name: {
-                "en": "",
-                "ru": "Создаем меч",
-            },
-            desc: {
-                "en": "",
-                "ru": "Ты уже не мало побегал по местным лесам, много животных видел, наверное. Некоторые из них довольно агрессивные. Чтобы драться с животными тебе понадобиться меч. ",
-            },
-            tip: {
-                "en": "",
-                "ru": "Купите у Шот суп",
-            },
-            final: {
-                "en": "",
-                "ru": "Из тебя получится неплохой воин, с таким то мечом!",
-            },
-        },
-        "hunger-1": {
-            name: {
-                "en": "",
-                "ru": "В Таверну, к Шот",
-            },
-            desc: {
-                "en": "",
-                "ru": "Собственно, это все, что тебе нужно сейчас знать о создании вещей, дальше сам разберешься. Ты кстати, проголадался наверное, пока бегал? Иди в трактир местный, там тебя покормят, может еще узнешь что интересное. ",
-            },
-            tip: {
-                "en": "",
-                "ru": "Таверна это большое заведение напротив Чарльза",
-            },
-            final: {
-                "en": "",
-                "ru": "Я всегда рада новым людям в нашем трактире, присаживайся, поговорим с тобой. ",
-            },
-        },
-        "hunger-2": {
-            name: {
-                "en": "",
-                "ru": "Голод, жажда",
-            },
-            desc: {
-                "en": "",
-                "ru": "Может перекусишь чего? Если долго ходить голодным, то силы твои будут исчезать, а потом и вовсе, кирдык... Лапы к верху... Брррр, ужас. А если добротно поесть, то силы будут сами к тебе потихоньку приходить. Только смотри не переусердствуй, а то знаешь как бывает: с полным животом от волка не убежишь! Так что вот, держи меню, можешь что-нибудь выбрать.",
-            },
-            tip: {
-                "en": "",
-                "ru": "Купите у Шот суп, сожрите. Если переедание - туалет неподалеку. Заодно научитесь срать, немаловажный навык выживания.",
-            },
-            final: {
-                "en": "",
-                "ru": "Ну что, наелся? То-то же, у нас тут все свежее, вкусное.",
-            },
-        },
-        "hunger-3": {
-            name: {
-                "en": "",
-                "ru": "Витамины",
-            },
-            desc: {
-                "en": "",
-                "ru": "Еда у нас в трактире вкусная, но не дает витаминов, а лишь энергию. А чтобы развивать в себе такие атрибуты, как сила или мудрость и еще другие, тебе необходимы витамины. А от атрибутов напрямую зависят твои навыки.",
-            },
-            tip: {
-                "en": "",
-                "ru": "тут должны быть ссылки на Атрибут и Пища",
-            },
-            final: {
-                "en": "",
-                "ru": "Ах да, забыла сказать, что немалое количество витаминов находится в выпечке всякой и мясе, если его грамотно приготовить.",
-            },
-        },
-        "tour-4": {
-            name: {
-                "en": "",
-                "ru": "К Чарльзу",
-            },
-            desc: {
-                "en": "",
-                "ru": "Теперь ты накормлен, напоен, пора и честь знать! Кстати, мне передал Чарльз, что он хочет тебя видеть. Так что иди к нему сейчас, но не забывай заглядывать к нам, буду рада видеть.",
-            },
-            tip: {
-                "en": "",
-                "ru": "",
-            },
-            final: {
-                "en": "",
-                "ru": "Рад снова видеть тебя! Освоился уже здесь? Никто не обижает? Это хорошо-хорошо. У нас в Рогалии леса полны дичи, в реках полно рыбы, а земля богата полезными ископаемыми.  Думаю, ты сможешь найти свое место в нашем мире.",
-            },
-        },
-        "tour-5": {
-            name: {
-                "en": "",
-                "ru": "Клайм",
-            },
-            desc: {
-                "en": "",
-                "ru": "Чтобы считаться гражданином Рогалии, тебе нужно выбрать гражданство и взять лицензию на клейм. Сделай это сейчас.",
-            },
-            tip: {
-                "en": "",
-                "ru": "Как взять лицензию на клейм: выбираем Чарльза->Установить гражданство(выбираем гражданство)->Получить клейм. Клейм можно расширить, если в банке есть деньги.",
-            },
-            final: {
-                "en": "",
-                "ru": "Отлично, теперь ты можешь поставить свой клайм. Налог нужно платить раз в неделю, не много, но все же лучше держи деньги на оплату в банке.",
-            },
-        },
-        "tour-6": {
-            name: {
-                "en": "",
-                "ru": "Священник Борис",
-            },
-            desc: {
-                "en": "",
-                "ru": "Ну вот, теперь ты гражданин Рогалии, поздравляю. Правда есть еще один момент, который тебе нужно знать... Вообще, иди к священнику Борису, он разъяснит.",
-            },
-            tip: {
-                "en": "",
-                "ru": "Что бы твой дом работал как надо, нужно помещать стены и ворота внутри твоего клайма.",
-            },
-            final: {
-                "en": "",
-                "ru": "Дитя Божее, все мы грешны, и кара Господня будет ждать тебя, если ты сделаешь что-то плохое. Например, убъешь другого человека... Ооо... Это страшный грех. Твой показатель кармы станет отрицательным и Иисус покарает тебя своей божественной силой.",
-            },
-        },
-        "tour-7": {
-            name: {
-                "en": "",
-                "ru": "Прощание",
-            },
-            desc: {
-                "en": "",
-                "ru": "Иисус всегда следит за тобой. Но по мере получения знаний, ты будешь отдаляться от Церкви и нашей защиты. Это связано с природой таких как ты, искусственно созданных Изгоев. Но пока не будем об этот. Отправляйся к Чарльзу.",
-            },
-            tip: {
-                "en": "",
-                "ru": "Если тебе убьют до 20лвл ты легко восстановишься со всем своими вещами либо в городе, либо в перерождателе.",
-            },
-            final: {
-                "en": "",
-                "ru": "Теперь ты все знаешь, все умеешь. Последний совет - сделай перерождатель и тогда ты всегда сможешь появится возле него, если вдруг умрешь. Удачи тебе!",
-            },
-        },
-        },
-    };
+function Quest(q) {
+    for (var i in q) {
+        this[i] = q[i];
+    }
+    this.data = game.quests[q.Id];
 }
+
+Quest.prototype = {
+    getName: function() {
+        return this.data.name[game.lang];
+    },
+    getLog: function() {
+        return game.player.ActiveQuests[this.Id];
+    },
+    active: function() {
+        return !!this.getLog();
+    },
+    ready: function() {
+        var questLog = this.getLog();
+        return questLog && questLog.State == "ready";
+    },
+    getStatusMarker: function() {
+        if (this.ready())
+            return "?";
+        if (this.active())
+            return "…";
+        return "!";
+    },
+    makeList: function(items) {
+        var list = document.createElement("div");
+        for (var item in items) {
+            var slot = document.createElement("div");
+            slot.classList.add("slot");
+            slot.appendChild(Entity.getPreview(item));
+            slot.onclick = game.controller.craft.makeSearch(item);
+
+            var desc = document.createElement("div");
+            var count = (items[item] instanceof Object) ? items[item].Count : items[item];
+            desc.textContent = TS(item) + ": x" + count;
+
+            var li = document.createElement("div");
+            li.className = "quest-item";
+            li.appendChild(slot);
+            li.appendChild(desc);
+            list.appendChild(li);
+        }
+        return list;
+    },
+    getDescContents: function(ready) {
+        return [
+            this.makeDesc(ready),
+            util.hr(),
+            this.makeGoal(),
+            util.hr(),
+            this.makeReward(),
+        ];
+    },
+    makeDesc: function(ready) {
+        var desc = document.createElement("div");
+        var source = (ready) ? this.data.final : this.data.desc;
+        desc.textContent = source[game.lang];
+        return desc;
+    },
+    makeGoal: function() {
+        var goal = document.createElement("div");
+
+        var end = document.createElement("div");
+        end.textContent = T("Who") + ": " + this.End;
+        goal.appendChild(end);
+
+        if (this.Goal.HaveItems) {
+            goal.appendChild(document.createTextNode(T("You need to have these items") + ":"));
+            goal.appendChild(this.makeList(this.Goal.HaveItems));
+        }
+        if (this.Goal.BringItems) {
+            goal.appendChild(document.createTextNode(T("You need to bring these items") + ":"));
+            goal.appendChild(this.makeList(this.Goal.BringItems));
+        }
+        if (this.Goal.Cmd) {
+            goal.appendChild(document.createTextNode(T("You need to") + ": " + TS(this.Goal.Cmd)));
+        }
+
+        if (this.data.tip) {
+            var tip = " (" + this.data.tip[game.lang] + ")";
+            goal.appendChild(document.createTextNode(tip));
+        }
+
+        return goal;
+    },
+    makeReward: function() {
+        var reward = document.createElement("div");
+        reward.appendChild(document.createTextNode(T("Rewards") + ": "));
+        if (this.Reward.Xp) {
+            var xp = document.createElement("div");
+            xp.textContent = "+" + this.Reward.Xp + "xp";
+            reward.appendChild(xp);
+        }
+        if (this.Reward.Currency) {
+            reward.appendChild(Vendor.createPrice(this.Reward.Currency));
+        }
+        if (this.Reward.Items) {
+            reward.appendChild(this.makeList(this.Reward.Items));
+        }
+
+        return reward;
+    },
+    getContents: function() {
+        var canStart = !this.active();
+        var action = (canStart) ? "Accept" : "Finish";
+
+        var button = document.createElement("button");
+        button.textContent = T(action);
+
+        var nearEndNpc = game.player.interactTarget.Name == this.End;
+        var canEnd = this.ready() && nearEndNpc;
+
+        if (canEnd || canStart) {
+            var self = this;
+            button.onclick = function() {
+                game.network.send("quest", {QuestId: self.Id}, function update(data) {
+                    if (!data.Done) {
+                        return update;
+                    }
+                    // we must clear AvailableQuests because
+                    // server will send us only non-empty quest list
+                    // so when we accept the last "available" quest
+                    // we have to manually clean up here
+                    delete game.player.AvailableQuests[self.Name];
+                    if (canEnd || !nearEndNpc)
+                        game.panels.quest.close();
+                    else
+                        game.panels.quest.setContents(self.getContents());
+                    return null;
+                });
+            };
+        } else {
+            button.disabled = true;
+        }
+
+        return this.getDescContents(canEnd).concat(
+            util.hr(),
+            button
+        );
+    },
+};
