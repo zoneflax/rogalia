7007dca2088c0be74dd5a25041730d6d49fe8e01
John <john.mclaren013@hotmail.com>
2017-01-09 00:18:21 +1300

    Minor spelling and punctuation fix (#101)

--- src/lang/en/dict.js
+++ src/lang/en/dict.js
@@ -2,7 +2,7 @@
 
 T.dict = {
     "To equip": "Equip",
-    "claim-help-text": "Extending claim territory automatically withdraw money from your bank account. Paid duration can be checked in the city's bank. Also there you can pay for a claim for a period from one week to three monthes.",
+    "claim-help-text": "Extending claim territory automatically withdraws money from your bank account. Paid duration can be checked in the city's bank. Also, there you can pay for a claim for a period from one week to three months.",
     "Wolf hungry": "Hungry wolf",
     "Wolf undead": "Undead wolf",
     "Wolf fatty": "Fat wolf",
