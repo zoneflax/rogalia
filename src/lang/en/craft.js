c7da981efb1957044311d19f730052c009805cf2
John <john.mclaren013@hotmail.com>
2017-01-09 00:18:39 +1300

    Minor spelling and grammar changes (#100)

--- src/lang/en/craft.js
+++ src/lang/en/craft.js
@@ -3,7 +3,7 @@
 Craft.help = {
     "stick": "To get a stick right click on a bough and select \"Break off\" option.",
     "twig": "To get a twig right click on a branch and select \"Break off\" option.",
-    "stone": "Stone can be \"chipped\" with a pickaxe from a boulder and stone block, or you can simply find one one the ground",
-    "leather": "Dry raw hide using drying frame. Then use tanning tub to make a leather from it." ,
+    "stone": "Stone can be \"chipped\" with a pickaxe from a boulder or stone block, or you can simply find one on the ground",
+    "leather": "Dry raw hides using a drying frame. Use tanning tub to make leather from the dried hide." ,
 
 };
