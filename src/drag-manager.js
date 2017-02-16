af1270c6475f2dbe2f5fe9517920a9cb90919c48
TatriX <tatrics@gmail.com>
2017-02-16 18:17:43 +0300

    Remove copy/paste

--- src/drag-manager.js
+++ src/drag-manager.js
@@ -40,112 +40,57 @@ function DragManager() {
         target.style.transform = `translate(${x}px, ${y}px)`;
     });
 
-    function snap(element, x, y) {
-        const pos = {x, y}
-        const closest = {
-            dx: +Infinity,
-            dy: +Infinity,
-        }
-        const margin = 2;
-        const threshold = 10;
-        const panels = Object.values(game.panels).concat([
-            {
-                x: game.offset.x,
-                y: game.offset.y,
-                width: game.world.offsetWidth - margin,
-                height: game.world.offsetHeight - margin,
-                visible: true,
-            },
-            {
-                x: 0,
-                y: 0,
-                width: window.innerWidth - margin,
-                height: window.innerHeight - margin,
-                visible: true,
-            },
-        ])
-        panels.forEach(function(panel) {
-            if (panel.element == element || !panel.visible) {
-                return;
-            }
-
-            if ((y < panel.y + panel.height + threshold) && (y + element.clientHeight > panel.y - threshold)) {
-                {
-                    const snapX = panel.x + panel.width + margin;
-                    const dx = Math.abs(snapX - x);
-                    if (dx < threshold && dx < closest.dx) {
-                        closest.dx = dx;
-                        pos.x = snapX;
-                    }
-                }
-                {
-                    const snapX = panel.x;
-                    const dx = Math.abs(snapX - x);
-                    if (dx < threshold && dx < closest.dx) {
-                        closest.dx = dx;
-                        pos.x = snapX;
-                    }
-                }
-                {
-                    const snapX = panel.x - element.clientWidth - margin;
-                    const dx = Math.abs(snapX - x);
-                    if (dx < threshold && dx < closest.dx) {
-                        closest.dx = dx;
-                        pos.x = snapX;
-                    }
-                }
-                {
-                    const snapX = panel.x + panel.width - element.clientWidth;
-                    const dx = Math.abs(snapX - x);
-                    if (dx < threshold && dx < closest.dx) {
-                        closest.dx = dx;
-                        pos.x = snapX;
-                    }
-                }
-            }
-            if ((x < panel.x + panel.width + threshold) && (x + element.clientWidth > panel.x - threshold)) {
-                {
-                    const snapY = panel.y + panel.height + margin;
-                    const dy = Math.abs(snapY - y);
-                    if (dy < threshold && dy < closest.dy) {
-                        closest.dy = dy;
-                        pos.y = snapY;
-                    }
-                }
-                {
-                    const snapY = panel.y;
-                    const dy = Math.abs(snapY - y);
-                    if (dy < threshold && dy < closest.dy) {
-                        closest.dy = dy;
-                        pos.y = snapY;
-                    }
-                }
-                {
-                    const snapY = panel.y - element.clientHeight - margin;
-                    const dy = Math.abs(snapY - y);
-                    if (dy < threshold && dy < closest.dy) {
-                        closest.dy = dy;
-                        pos.y = snapY;
-                    }
-                }
-                {
-                    const snapY = panel.y + panel.height - element.clientHeight;
-                    const dy = Math.abs(snapY - y);
-                    if (dy < threshold && dy < closest.dy) {
-                        closest.dy = dy;
-                        pos.y = snapY;
-                    }
-                }
-            }
-        });
-        return pos;
-    }
-
     function dragIgnore(element) {
         if (dragIgnoreTags.indexOf(element.tagName) != -1)
             return true;
 
         return !document.defaultView.getComputedStyle(element).cursor.includes("default");
-    };
+    }
 
+    function snap(element, x, y, threshold = 10, margin = 2) {
+        const pos = {x, y}
+        const closest = {
+            dx: +Infinity,
+            dy: +Infinity,
+        }
+        const width = element.clientWidth;
+        const height = element.clientHeight;
+        Object.values(game.panels)
+            .filter(panel => panel.visible && panel.element != element)
+            .concat([
+                {
+                    x: game.offset.x,
+                    y: game.offset.y,
+                    width: game.world.offsetWidth - margin,
+                    height: game.world.offsetHeight - margin,
+                },
+                {
+                    x: 0,
+                    y: 0,
+                    width: window.innerWidth - margin,
+                    height: window.innerHeight - margin,
+                },
+            ]).forEach(function(panel) {
+                if ((y < panel.y + panel.height + threshold) && (y + height > panel.y - threshold)) {
+                    [closest.dx, pos.x] = edge(panel.x + panel.width + margin, x, closest.dx, pos.x);
+                    [closest.dx, pos.x] = edge(panel.x, x, closest.dx, pos.x);
+                    [closest.dx, pos.x] = edge(panel.x - width - margin, x, closest.dx, pos.x);
+                    [closest.dx, pos.x] = edge(panel.x + panel.width - width, x, closest.dx, pos.x);
+                }
+                if ((x < panel.x + panel.width + threshold) && (x + width > panel.x - threshold)) {
+                    [closest.dy, pos.y] = edge(panel.y + panel.height + margin, y, closest.dy, pos.y);
+                    [closest.dy, pos.y] = edge(panel.y, y, closest.dy, pos.y);
+                    [closest.dy, pos.y] = edge(panel.y - height - margin, y, closest.dy, pos.y);
+                    [closest.dy, pos.y] = edge(panel.y + panel.height - height, y, closest.dy, pos.y);
+                }
+            });
+        return pos;
+
+        function edge(snap, p, closest, pos) {
+            const delta = Math.abs(snap - p);
+            return (delta < threshold && delta < closest)
+                ? [delta, snap]
+                : [closest, pos];
+        }
+    }
 };
